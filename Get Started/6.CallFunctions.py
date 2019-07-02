c7399077f7c1680493ee6ae3e3f8402dcf1c66f1
qinhong.wang <qinhong.wang@nnlife.co.jp>
2019-07-02 18:25:57 +0900

    1

---
+++ Get Started/6.CallFunctions.py
@@ -0,0 +1,20 @@
+#--------------------------------
+# 1. Call In-Build Functions
+#--------------------------------
+# abs()
+print(abs(100))
+print(abs(-20))
+print(abs(-3.14))
+# max()
+print(max(1, 2))
+print(max(1, 3, 5, 7, 9))
+# type conventer
+print(int("33"))
+print(float("12.34"))
+print(str(1.23))
+print(bool(0))
+print(bool(""))
+
+a = abs
+print(a(-1))
+
--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,3 @@
-s = input("\n\nsay some words:")
-print(s)
-if s.endswith("a"):
-    print("Hello too")
-else:
-    print("Bye~~~")
-    print("emmmmm")
+from MyFunction import my_abs
+
+print(my_abs(-100))
