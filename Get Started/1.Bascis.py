3655bda425c83f23a030b3d82ea9ca6fcdfc4a57
qinhong.wang <qinhong.wang@nnlife.co.jp>
2019-07-02 16:41:31 +0900

    1

---
+++ Get Started/1.Bascis.py
@@ -0,0 +1,42 @@
+#--------------------------------
+# 1. 数据类型和变量
+#--------------------------------
+
+# 整数
+number_1 = 1
+number_2 = 2
+print(number_1 + number_2)
+
+# 浮点数
+float_1 = 3.14
+print(float_1)
+
+# 字符串
+str_1 = "I\'m \"OK\""
+str_2 = '''
+It is a long
+long long
+long
+story...
+'''
+print(str_1)
+print(str_2)
+
+# 布尔值
+print(True and False)
+print(True or False)
+print(not True)
+
+# 空值
+print(None)
+
+# 变量
+# 动态语言：类型不固定
+# 静态语言： 类型固定
+a = 1
+t_007 = "T007"
+Answer = True
+
+# 常量
+# 全部大写的变量名表示常量
+PI = 3.1415926
